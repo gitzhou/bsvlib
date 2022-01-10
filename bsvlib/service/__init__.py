f6b46f75fed5ca7f17894c60d0c19d67f1c329bb
aaron67 <aaron67@aaron67.cc>
2022-01-11 05:57:46 +0800

    Add new service provider - sensible query
    Signed-off-by: aaron67 <aaron67@aaron67.cc>

--- bsvlib/service/__init__.py
+++ bsvlib/service/__init__.py
@@ -1,3 +1,4 @@
 from .provider import Provider
+from .sensiblequery import SensibleQuery
 from .service import Service
 from .whatsonchain import WhatsOnChain
---
+++ bsvlib/service/sensiblequery.py
@@ -0,0 +1,54 @@
+import json
+from contextlib import suppress
+from typing import Optional, List, Dict
+
+import requests
+
+from .provider import Provider
+from ..keys import PrivateKey
+from ..script.type import P2pkScriptType
+
+
+class SensibleQuery(Provider):
+
+    def __init__(self):
+        self.url: str = 'https://api.sensiblequery.com'
+        self.headers: Dict = {'Content-Type': 'application/json', 'Accept': 'application/json', }
+        self.timeout: int = 30
+
+    def get_unspents(self, **kwargs) -> List[Dict]:
+        """
+        P2PKH and P2PK unspents
+        """
+        with suppress(Exception):
+            private_key: PrivateKey = kwargs.get('private_keys')[0]
+            params = {'cursor': 0, 'size': 5120}
+            r = requests.get(f'{self.url}/address/{private_key.address()}/utxo', headers=self.headers, params=params, timeout=self.timeout)
+            r.raise_for_status()
+            unspents: List[Dict] = []
+            for item in r.json()['data']:
+                unspent = {'txid': item['txid'], 'vout': item['vout'], 'satoshi': item['satoshi'], 'height': item['height']}
+                if item['scriptType'] == '21ac':
+                    unspent['script_type'] = P2pkScriptType()
+                    unspent['locking_script'] = P2pkScriptType.locking(private_key.public_key().serialize())
+                unspent.update(kwargs)
+                unspents.append(unspent)
+            return unspents
+        return []
+
+    def get_balance(self, **kwargs) -> int:
+        with suppress(Exception):
+            private_key: PrivateKey = kwargs.get('private_keys')[0]
+            r = requests.get(f'{self.url}/address/{private_key.address()}/balance', headers=self.headers, timeout=self.timeout)
+            r.raise_for_status()
+            balance: dict = r.json()
+            return balance.get('satoshi') + balance.get('pendingSatoshi')
+        return 0
+
+    def broadcast(self, raw: str) -> Optional[str]:
+        with suppress(Exception):
+            data = json.dumps({'txHex': raw})
+            r = requests.post(f'{self.url}/pushtx', headers=self.headers, data=data, timeout=self.timeout)
+            r.raise_for_status()
+            return r.json()['data']
+        return None
