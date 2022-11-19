9ae2323723c4b53f7807ef5cdcc86670fdcea483
aaron67 <aaron67@aaron67.cc>
2022-11-20 04:15:27 +0800

    Upgrade requirements
    Signed-off-by: aaron67 <aaron67@aaron67.cc>

--- tests/test_service.py
+++ tests/test_service.py
@@ -11,12 +11,3 @@ def test_service():
         assert isinstance(Service().provider, WhatsOnChain)
 
     assert isinstance(Service(chain=Chain.TEST).provider, WhatsOnChain)
-
-
-def test_metasv():
-    if METASV_TOKEN:
-        service = MetaSV(token=METASV_TOKEN)
-        address = '13LGR1QjYkdi4adZV1Go6cQTxFYjquhS1y'
-        unspents = service.get_unspents(address=address)
-        balance = service.get_balance(address=address)
-        assert sum([unspent['satoshi'] for unspent in unspents]) == balance
