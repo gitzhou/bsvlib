c354225779d470caf16d1d67446c69de204ee1a5
aaron67 <aaron67@aaron67.cc>
2022-11-26 00:36:31 +0800

    Add function export
    Signed-off-by: aaron67 <aaron67@aaron67.cc>

--- bsvlib/__init__.py
+++ bsvlib/__init__.py
@@ -1,6 +1,6 @@
 from .aes import InvalidPadding
 from .keys import verify_signed_text, Key, PublicKey, PrivateKey
 from .transaction import TxInput, TxOutput, Transaction, Unspent, InsufficientFunds
-from .wallet import Wallet
+from .wallet import Wallet, create_transaction
 
 __version__ = '0.9.0'
