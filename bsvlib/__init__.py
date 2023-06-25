f3b70f3a3429da9be99e9af2981bb8e43470eebd
aaron67 <aaron67@aaron67.cc>
2023-06-26 00:19:53 +0800

    Upgrade to v0.10.0
    Signed-off-by: aaron67 <aaron67@aaron67.cc>

--- bsvlib/__init__.py
+++ bsvlib/__init__.py
@@ -3,4 +3,4 @@ from .keys import verify_signed_text, Key, PublicKey, PrivateKey
 from .transaction import TxInput, TxOutput, Transaction, Unspent, InsufficientFunds
 from .wallet import Wallet, create_transaction
 
-__version__ = '0.9.0'
+__version__ = '0.10.0'
